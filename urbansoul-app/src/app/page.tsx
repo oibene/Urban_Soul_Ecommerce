1dd3af3331754c93b221cce23942b358faaaa783
oibene <alvespbene@gmail.com>
2025-09-16 20:42:09 -0300

    footer

--- urbansoul-app/src/app/page.tsx
+++ urbansoul-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 import './global.css'
 
 import Header from "./pages/header"
+import Footer from "./pages/footer"
 
 export default function Page() {
     return (
         <div className="bg-bglight">
             <Header></Header>
+            <Footer></Footer>
         </div>
         
     )
